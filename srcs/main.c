117282f3bf1571bf3225d2790b3badbc2b1d0792
viruskizz <kizzaraiva@gmail.com>
2023-02-23 23:28:38 +0700

    save

--- srcs/main.c
+++ srcs/main.c
@@ -14,12 +14,13 @@
 
 static void	initial(t_data *data);
 static int	render(t_data *data);
+t_obj	*set_plain_img(t_data *data, t_obj *obj);
 
 int main(int argc, char **argv)
 {
 	t_data	data;
 	char *filename;
-	
+
 	data = parsing_input(argc, argv);
 	print_tdata(&data);
 	initial(&data);
@@ -37,59 +38,70 @@ static void	initial(t_data *data)
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "miniRT");
 	if (!data->win)
 		error_exit(data, ERROR_WIN);
-	t_img	img;
-	img.img = mlx_new_image(data->mlx, data->w, data->h);
-	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.lh, &img.endian);
-	data->scene.img = img;
+	// t_img	img;
+	// img.img = mlx_new_image(data->mlx, data->w, data->h);
+	// img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.lh, &img.endian);
+	// data->scene.img = img;
+	// data->scene.img.img = mlx_new_image(data->mlx, data->w, data->h);
+	data->scene.color.r = 128;
+	data->scene.color.g = 128;
+	data->scene.color.b = 128;
+	data->scene.pos.x = 0;
+	data->scene.pos.y = 0;
+	data->scene = *set_plain_img(data, &data->scene);
+}
+
+static int	render(t_data *data)
+{
+	// render_objects(data);
+	mlx_put_image_to_window(data->mlx, data->win, data->scene.img.img, 0, 0);
+	return (0);
 }
 
 // static int	render(t_data *data)
 // {
-// 	render_objects(data);
+// 	int width = data->w;
+// 	int height = data->h;
+// 	t_ray	camray;
+// 	t_vtr	inter_point;
+// 	t_vtr	local_normal;
+// 	t_vtr	local_color;
+// 	double x_fact = 1.0 / ((double)data->w / 2.0);
+// 	double y_fact = 1.0 / ((double)data->h / 2.0);
+// 	double min_d = 1e6;
+// 	double max_d = 0.0;
+// 	int	x = 0;
+// 	int	y = 0;
+// 	// if (data->scene.img.img)
+// 	// 	mlx_destroy_image(data->mlx, data->scene.img.img);
+// 	while (y < data->h)
+// 	{
+// 		while (x < data->w)
+// 		{
+// 			// normalize the x and y coordinates
+// 			// printf("fact: %f, %f\n", x_fact, y_fact);
+// 			double norm_x = ((double) x * x_fact) - 1.0;
+// 			double norm_y = ((double) y * y_fact) - 1.0;
+// 			// printf("norm: %f, %f\n", norm_x, norm_y);
+// 			camray = raygen(data->cam, norm_x, norm_y);
+// 			int inter = sphere_inter(camray);
+// 			int color;
+// 			if (inter)
+// 			{
+// 				color = rgb_to_int(255, 0, 0);
+// 			}
+// 			else
+// 			{
+// 				color = rgb_to_int(255, 0, 0);
+// 			}
+// 			pixel_put_img(&data->scene.img, x, y, color);
+// 			x++;
+// 		}
+// 		y++;
+// 	}
+// 	mlx_put_image_to_window(data->mlx, data->win, data->scene.img.img, 10, 10);
 // 	return (0);
 // }
-
-static int	render(t_data *data)
-{
-	int width = data->w;
-	int height = data->h;
-	t_ray	camray;
-	t_vtr	inter_point;
-	t_vtr	local_normal;
-	t_vtr	local_color;
-	double x_fact = 1.0 / ((double)data->w / 2.0);
-	double y_fact = 1.0 / ((double)data->h / 2.0);
-	double min_d = 1e6;
-	double max_d = 0.0;
-	int	x = 0;
-	int	y = 0;
-	while (x < data->w)
-	{
-		while (y < data->h)
-		{
-			// normalize the x and y coordinates
-			printf("fact: %f, %f\n", x_fact, y_fact);
-			double norm_x = ((double) x * x_fact) - 1.0;
-			double norm_y = ((double) y * y_fact) - 1.0;
-			printf("norm: %f, %f\n", norm_x, norm_y);
-			camray = raygen(data->cam, norm_x, norm_y);
-			int inter = sphere_inter(camray);
-			int color;
-			if (inter)
-			{
-				color = rgb_to_int(255, 0, 0);
-			}
-			else
-			{
-				color = rgb_to_int(255, 0, 0);
-			}
-			pixel_put_img(&data->scene.img, x, y, color);
-			y++;
-		}
-		x++;
-	}
-	return (0);
-}
 
 // t_obj	*set_plain_img(t_data *data, t_obj *obj)
 // {
