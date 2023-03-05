e8317f4a1dc8338231adda95482fe9e51fb22645
araiva <kizzaraiva@gmail.com>
2023-03-06 01:15:30 +0700

    clear non using code

--- srcs/render.c
+++ srcs/render.c
@@ -6,8 +6,7 @@ int render_scene(t_data *data)
 {
 	t_ray	camray;
 	t_ints	ints;
-	// if (data->scene.img.ptr)
-	// 	mlx_destroy_image(data->mlx, data->scene.img.ptr);
+
 	int y;
 	int x;
 	y = 0;
@@ -20,23 +19,12 @@ int render_scene(t_data *data)
 			float cox = (x / data->scene.pos.x) - 1.0;
 			float coy = (y / data->scene.pos.y) - 1.0;
 			// float coy = 1.0 - (y / data->scene.pos.y);
-			if (x == data->w / 2.0 + 100 && y == data->h / 2.0 + 100)
-			{
-				printf("x = %d / %f, %f\n", x, data->w / 2.0, cox);
-				printf("y = %d / %f, %f\n", y, data->h / 2.0, coy);
-			}
-			if (x == data->w / 2.0 - 100 && y == data->h / 2.0 - 100)
-			{
-				printf("x = %d / %f, %f\n", x, data->w / 2.0, cox);
-				printf("y = %d / %f, %f\n", y, data->h / 2.0, coy);
-			}
 			camray = cam_ray(data->cam, cox, coy);
 			t_list *obj;
 			t_list *tmp;
 
 			obj = data->objs;
 			tmp = obj;
-			int inter = 0;
 			int color = rgb_to_int(0, 0, 0);
 			while (tmp)
 			{
@@ -45,18 +33,17 @@ int render_scene(t_data *data)
 					t_obj *o = (t_obj *)tmp->content;
 					if (o->type == SPHERE)
 						sphere_inter(o, camray, &ints);
+					else if (o->type == CYLIND)
+						printf("CYLIND\n");
 					if (ints.valid)
 					{
 						// t_color c = color_inter(data, camray, &ints);
-						// printf("(%d,%d) c: %d,%d,%d\n", x, y, c.r, c.g, c.b);
 						color = color_to_int(color_inter(data, camray, &ints));
 					}
 					else
 					{
 						// color = rgb_to_int(0, 0, 0);
-						// color = rgb_to_int(0, 255, 0);
 					}
-					// printf("%d,%d = %d\n", x, y, 0xFF00);
 					pixel_put_img(&data->scene.img, x, y, color);
 				}
 				tmp = tmp->next;
--- srcs/transform/trans_homo.c
+++ srcs/transform/trans_homo.c
@@ -16,6 +16,8 @@ static void		trans_trans(t_vtr trans, t_vtr scale, float ***result);
 static float	**trans_scale(t_vtr scale);
 static float	**trans_move(t_vtr trans);
 
+// Transform Homogenous
+// ! Get A contanst
 float	**trans_homo(t_vtr trans, t_vtr rotate, t_vtr scale)
 {
 	float	**result;
@@ -23,13 +25,7 @@ float	**trans_homo(t_vtr trans, t_vtr rotate, t_vtr scale)
 	float	**mrot;
 
 	trans_trans(trans, scale, &mtrans);
-	// vtrprint(trans);
-	// vtrprint(rotate);
 	trans_rot_xyz(rotate, &mrot);
-	// printf("mtrans\n");
-	// mtx_print(mtrans, 4);
-	// printf("mrot\n");
-	// mtx_print(mrot, 4);
 	result = mtx_multi(mtrans, mrot, 4);
 
 	mtx_free(mtrans, 4);
