51b9f0ea063c13fc0e62a0d63d95a97d95bfabf3
Shivalux Harnvongsa <shivarakii@Shivaluxs-MacBook-Pro.local>
2023-03-25 13:31:51 +0700

    fix: mouse(add x, y)

--- srcs/event/input_keyhandler.c
+++ srcs/event/input_keyhandler.c
@@ -6,7 +6,7 @@
 /*   By: sharnvon <sharnvon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 21:54:53 by sharnvon          #+#    #+#             */
-/*   Updated: 2023/03/23 17:05:52 by sharnvon         ###   ########.fr       */
+/*   Updated: 2023/03/25 13:30:55 by sharnvon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,8 +94,10 @@ int	keyhandler_release(int keycode, t_data *data)
 /*
 * [handler mouse function]
 */
-int	mouse_hook(int keycode, t_data *data)
+int	mouse_hook(int keycode, int x, int y, t_data *data)
 {
+	(void) x;
+	(void) y;
 	if (keycode == 4)
 		window_interface(data, DECREASE);
 	else if (keycode == 5)
